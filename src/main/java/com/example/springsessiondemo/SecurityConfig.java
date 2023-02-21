4fd106293a526e353ed485ed95fce605b624ba00
Ashish Sharda <ashishjsharda@gmail.com>
2023-02-21 06:14:04 -0600

    add filter

--- src/main/java/com/example/springsessiondemo/SecurityConfig.java
+++ src/main/java/com/example/springsessiondemo/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.example.springsessiondemo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -14,5 +16,16 @@ public class SecurityConfig {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager.createUser(org.springframework.security.core.userdetails.User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
         return manager;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.httpBasic()
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .hasRole("USER").and().csrf().disable();
+
+        return httpSecurity.build();
     }
 }
