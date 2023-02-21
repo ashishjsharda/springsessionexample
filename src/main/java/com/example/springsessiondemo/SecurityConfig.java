312290cab3800abb7048926be1e5fbbe3c6a35d4
Ashish Sharda <ashishjsharda@gmail.com>
2023-02-21 06:08:45 -0600

    add userdetails,update build

--- src/main/java/com/example/springsessiondemo/SecurityConfig.java
+++ src/main/java/com/example/springsessiondemo/SecurityConfig.java
@@ -1,4 +1,18 @@
 package com.example.springsessiondemo;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(org.springframework.security.core.userdetails.User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
+        return manager;
+    }
 }
