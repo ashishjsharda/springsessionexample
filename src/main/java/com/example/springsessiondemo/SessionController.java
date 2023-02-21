767f6a249eb532b5c07a47b88909e72fb4416608
Ashish Sharda <ashishjsharda@gmail.com>
2023-02-21 06:01:15 -0600

    add controller

--- src/main/java/com/example/springsessiondemo/SessionController.java
+++ src/main/java/com/example/springsessiondemo/SessionController.java
@@ -1,4 +1,12 @@
 package com.example.springsessiondemo;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SessionController {
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World";
+    }
 }
